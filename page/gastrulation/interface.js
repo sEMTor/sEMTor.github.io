b1618b63a1e96093a6e02848332adc8278ca17f1
Steffen Plunder <steffen.plunder@web.de>
2024-02-07 03:07:37 +0900

    current state

--- page/gastrulation/interface.js
+++ page/gastrulation/interface.js
@@ -1,5 +1,6 @@
-
-var $ = function (id) { return document.getElementById(id); };
+var $ = function (id) {
+  return document.getElementById(id);
+};
 
 const params = {
   general: {
@@ -9,14 +10,14 @@ const params = {
     N_init: 59,
     N_max: 80,
     N_emt: 19,
-    w_init: 60,  // ~ N_init * 1.5
+    w_init: 60, // ~ N_init * 1.5
     h_init: 5,
     mu: 0.2,
     n_substeps: 30,
     alg_dt: 0.01,
     w_screen: 70,
-    h_screen: 70 * 1/5, 
-    p_div_out: 0.95,  // cp. NC
+    h_screen: (70 * 1) / 5,
+    p_div_out: 0.95, // cp. NC
   },
   cell_prop: {
     apical_junction_init: 1.5,
@@ -24,11 +25,10 @@ const params = {
     basal_daming_ratio: 1.0,
     cytos_init: 1.5,
     diffusion: 0.1,
-
   },
   cell_types: {
     control: {
-      name: 'control',
+      name: "control",
       R_hard: 0.4,
       R_hard_div: 0.7,
       R_soft: 1.2,
@@ -51,18 +51,18 @@ const params = {
       INM: 1.0,
       hetero: false,
       events: {
-        time_A: { min: Infinity, max: Infinity},
-        time_B: { min: Infinity, max: Infinity},
-        time_S: { min: Infinity, max: Infinity},
+        time_A: { min: Infinity, max: Infinity },
+        time_B: { min: Infinity, max: Infinity },
+        time_S: { min: Infinity, max: Infinity },
         time_P: { min: Infinity, max: Infinity },
-      }
+      },
     },
     emt: {
-      name: 'emt',
+      name: "emt",
       R_hard: 0.4,
       R_hard_div: 0.7,
       R_soft: 1.2,
-      color: {r: 128, g: 0, b: 128},
+      color: { r: 128, g: 0, b: 128 },
       dur_G2: 0.5,
       dur_mitosis: 0.5,
       k_apical_junction: 1.0,
@@ -78,39 +78,36 @@ const params = {
       stiffness_repulsion: 4.0,
       stiffness_straightness: 15.0,
       lifespan: { min: 5.5, max: 6.5 },
-      INM: 0.0,
+      INM: 1.0,
       hetero: false,
       events: {
         time_A: { min: Infinity, max: Infinity },
         time_B: { min: 6, max: 24 },
         time_S: { min: Infinity, max: Infinity },
         time_P: { min: Infinity, max: Infinity },
-      }
-    }
-  }
-}
+      },
+    },
+  },
+};
 
 function init_display(s) {
-    const pg = params.general;
-    const emt = params.cell_types.emt;  
+  const pg = params.general;
+  const emt = params.cell_types.emt;
 
-    function update_label(t, el) {
-      if( t < pg.t_end && !emt.hetero )
-        el.style = "left: " + String(t / pg.t_end * 100) + "%; display: block;";
-    else
-        el.style = "display: none;"
-    } 
+  function update_label(t, el) {
+    if (t < pg.t_end && !emt.hetero)
+      el.style = "left: " + String((t / pg.t_end) * 100) + "%; display: block;";
+    else el.style = "display: none;";
+  }
 
-    update_label(emt.events.time_A.min, p_A);
-    update_label(emt.events.time_B.min, p_B);
-    update_label(emt.events.time_S.min, p_S);
+  update_label(emt.events.time_A.min, p_A);
+  update_label(emt.events.time_B.min, p_B);
+  update_label(emt.events.time_S.min, p_S);
 
-    if( emt.run > 0 )
-        p_B.innerHTML  = "B(p)"
-    else
-        p_B.innerHTML  = "B"
+  if (emt.run > 0) p_B.innerHTML = "B(p)";
+  else p_B.innerHTML = "B";
 
-    p_sim_end.style = "display:none;"
+  p_sim_end.style = "display:none;";
 }
 
 const p_time = $("time");
@@ -122,47 +119,67 @@ const p_sim_end = $("sim_end");
 const p_info = $("timing_table");
 
 function update_display(p, s) {
-    const pg = params.general;
-    p_time.innerHTML = "" + String(s.t.toFixed(1)) + "h"; 
-    p_prog.style = "width: " + String(s.t / params.general.t_end * 100) + "%";
+  const pg = params.general;
+  p_time.innerHTML = "" + String(s.t.toFixed(1)) + "h";
+  p_prog.style = "width: " + String((s.t / params.general.t_end) * 100) + "%";
 
-    if( s.cells.length == params.general.N_max) {
-         p.fill(0,0,0);
-         p.text("Maximal number of cells reached. Cell division inactive.", -pg.w_screen/2 + 4, 0.2*pg.h_screen);
-    }
+  if (s.cells.length == params.general.N_max) {
+    p.fill(0, 0, 0);
+    p.text(
+      "Maximal number of cells reached. Cell division inactive.",
+      -pg.w_screen / 2 + 4,
+      0.2 * pg.h_screen
+    );
+  }
 
-    if( s.t > pg.t_end ) {
-      p_sim_end.style = "display:block;"
-    }
-};
+  if (s.t > pg.t_end) {
+    p_sim_end.style = "display:block;";
+  }
+}
 
 let inputs = {
   params: params,
   init_display: init_display,
   update_display: update_display,
-  screen_size: { w: 1000.0, h: 200.0 }
+  screen_size: { w: 1000.0, h: 200.0 },
 };
 
-let sim_emt_p5 = new p5(p => sim_emt(p, inputs, $('sim_div')), 'sim_div');
-
-// pcontrol.run = false;
-// pcontrol.INM = true;
-// pcontrol.A = 9;
-// pcontrol.B = 15;
-// pcontrol.S = 21;
-// pcontrol.N = 11;
+let sim_emt_p5 = new p5((p) => sim_emt(p, inputs, $("sim_div")), "sim_div");
 
 function controlFromHtml() {
-
   const pg = params.general;
-  const emt = params.cell_types.emt;
+  const epi = params.cell_types.control;
+  const ps = params.cell_types.emt;
 
-  emt.events.time_B = parseInt($("B_input").value) == 1 ? {min: 6, max: 24} : {min: Infinity, max: Infinity};
+  ps.events.time_B = !$("ps_attached").checked
+    ? { min: 6, max: 24 }
+    : { min: Infinity, max: Infinity };
+
+  ps.lifespan = $("ps_cc_duration") ? { min: 5.5, max: 6.5 } : { min: 2.5, max: 3.5}
+  epi.lifespan = $("epi_cc_duration") ? { min: 5.5, max: 6.5 } : { min: 2.5, max: 3.5}
+
+  ps.dur_G2 = $("epi_div_duration") ? 0.5 : 0.25
+  epi.dur_G2 = $("epi_div_duration") ? 0.5 : 0.25
+
+  ps.dur_mitosis = $("epi_div_duration") ? 0.5 : 0.25
+  epi.dur_mitosis = $("epi_div_duration") ? 0.5 : 0.25
+
+  // get value of  epi_contract radio inputs 
+  let epi_contract_value = JSON.parse(document.querySelector('input[name="epi_contract"]:checked').value);
+  let ps_contract_value = JSON.parse(document.querySelector('input[name="ps_contract"]:checked').value);
+  
+  epi.stiffness_nuclei_apical = epi_contract_value[0];
+  epi.stiffness_nuclei_basal = epi_contract_value[1];
+  epi.stiffness_apical_apical = epi_contract_value[2];
+
+  ps.stiffness_nuclei_apical = ps_contract_value[0];
+  ps.stiffness_nuclei_basal = ps_contract_value[1];
+  ps.stiffness_apical_apical = ps_contract_value[2];
 
   sim_emt_p5.init();
 }
 
-// controlFromHtml();
+controlFromHtml();
 
 // when run_button is pressed, call sim_emt.init()
 $("run_button").addEventListener("click", function () {
@@ -173,6 +190,16 @@ $("play_button").addEventListener("click", function () {
   sim_emt_p5.pause();
 });
 
-$("ps_attached").addEventListener("change", function () {
-    controlFromHtml();
-});
+$("ps_attached").addEventListener("change", controlFromHtml);
+$("ps_cc_duration").addEventListener("change", controlFromHtml);
+$("epi_cc_duration").addEventListener("change", controlFromHtml);
+$("ps_div_duration").addEventListener("change", controlFromHtml);
+$("epi_div_duration").addEventListener("change", controlFromHtml);
+$("epi_ct_all").addEventListener("click", controlFromHtml);
+$("ps_ct_all").addEventListener("click", controlFromHtml);
+$("epi_ct_inm").addEventListener("click", controlFromHtml);
+$("ps_ct_inm").addEventListener("click", controlFromHtml);
+$("epi_ct_api").addEventListener("click", controlFromHtml);
+$("ps_ct_api").addEventListener("click", controlFromHtml);
+$("epi_ct_ctrl").addEventListener("click", controlFromHtml);
+$("ps_ct_ctrl").addEventListener("click", controlFromHtml);

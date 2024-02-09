17ab4a17ec9e03e1419f777237d42fb1765a63ff
Steffen Plunder <steffen.plunder@web.de>
2024-02-09 20:28:48 +0900

    added images

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = config => {
     config.addPassthroughCopy('./page/js');
     config.addPassthroughCopy('./page/gastrulation/*.js');
     config.addPassthroughCopy('./page/neuralcrest/*.js');
+    config.addPassthroughCopy('./page/imgs/*.png');
 
     return {
       markdownTemplateEngine: 'njk',
